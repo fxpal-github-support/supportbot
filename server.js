38e1e5bea59b9e4cb222c4497f0c208bbb86a88f
Yulius Tjahjadi <yulius@fxpal.com>
2018-06-14 14:28:54 -0700

    fix missing username case

--- server.js
+++ server.js
@@ -23,50 +23,57 @@ const transporter = nodemailer.createTransport({
     path: '/usr/sbin/sendmail'
 });
 
+rtm.on('message', (message) => {
+    // For structure of `event`, see https://api.slack.com/events/message
+
+    // Skip messages that are from a bot or my own user ID
+    if ((message.subtype && message.subtype === 'bot_message') ||
+        (!message.subtype && message.user === rtm.activeUserId)) {
+        return;
+    }
+
+    // Log the message
+    //console.log(`(channel:${message.channel}) ${user[message.user].name} says: ${message.text}`);
+
+    // setup email data with unicode symbols
+    let username = (user[message.user] || {}).name;
+    if (!username) {
+        web.users.list()
+            .then((res) => {
+                console.log(res);
+                // update the users roster
+                res.members.forEach(m => user[m.id] = m);
+                username = (user[message.user] || {}).name;
+                sendEmail(username, message.text);
+            })
+            .catch(console.error);
+    } else {
+        sendEmail(username, message.text);
+    }
+});
+
 web.users.list()
     .then((res) => {
         console.log(res);
         res.members.forEach(m => user[m.id] = m);
-
-        rtm.on('message', (message) => {
-            // For structure of `event`, see https://api.slack.com/events/message
-
-            // Skip messages that are from a bot or my own user ID
-            if ((message.subtype && message.subtype === 'bot_message') ||
-                (!message.subtype && message.user === rtm.activeUserId)) {
-                return;
-            }
-
-            // Log the message
-            //console.log(`(channel:${message.channel}) ${user[message.user].name} says: ${message.text}`);
-
-            // setup email data with unicode symbols
-            let username = (user[message.user] || {}).name || message.user;
-            if (!username) {
-                return;
-            }
-
-            let mailOptions = {
-                from: '"SupportBot" <support@fxpal.com>', // sender address
-                to: 'support@fxpal.com', // list of receivers
-                subject: 'From Slack: #support', // Subject line
-                text: `${username} says: ${message.text}`, // plain text bod
-            };
-
-
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    return console.log(error);
-                }
-                // console.log('Message sent: %s', info.messageId);
-                // // Preview only available when sending through an Ethereal account
-                // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-            });
-        });
-
     })
     .catch(console.error);
 
+
+
+function sendEmail(username, text) {
+    let mailOptions = {
+        from: '"SupportBot" <support@fxpal.com>', // sender address
+        to: 'support@fxpal.com', // list of receivers
+        subject: 'From Slack: #support', // Subject line
+        text: `${username} says: ${text}`, // plain text bod
+    };
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        // console.log('Message sent: %s', info.messageId);
+    });
+}
