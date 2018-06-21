6db5a99feb3d88c045c79ebed99e10d07e31663b
Yulius Tjahjadi <yulius@fxpal.com>
2018-06-21 16:38:49 -0700

    add better messaging format

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 // An access token (from your Slack app or custom integration - usually xoxb)
-const token = process.env.SLACK_TOKEN;
+const webtoken = process.env.WEB_SLACK_TOKEN;
+const bottoken = process.env.BOT_SLACK_TOKEN;
+const mailTo = process.env.MAILTO || 'support@fxpal.com';
+const mailFrom = process.env.MAILFROM || '"SupportBot" <support@fxpal.com>';
+
+console.log(webtoken, bottoken, mailTo, mailFrom);
 
 // The client is initialized and then started to get an active connection to the platform
-const rtm = new RTMClient(token);
+const rtm = new RTMClient(bottoken);
 rtm.start();
 
-const web = new WebClient(token);
+const web = new WebClient(webtoken);
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-let supportChannel = '';
-let user = {}
+let user = {};
+let channel = {};
 
 // setup mail
 const transporter = nodemailer.createTransport({
@@ -35,41 +40,57 @@ rtm.on('message', (message) => {
     // Log the message
     //console.log(`(channel:${message.channel}) ${user[message.user].name} says: ${message.text}`);
 
-    // setup email data with unicode symbols
-    let username = (user[message.user] || {}).name;
-    if (!username) {
-        web.users.list()
-            .then((res) => {
-                console.log(res);
-                // update the users roster
-                res.members.forEach(m => user[m.id] = m);
-                username = (user[message.user] || {}).name;
-                sendEmail(username, message.text);
-            })
-            .catch(console.error);
-    } else {
-        sendEmail(username, message.text);
-    }
+    let userinfo = {};
+    getUserInfo(message.user)
+        .then((profile) => {
+            userinfo = profile;
+            return getChanelname(message.channel);
+        })
+        .then((name) => {
+            channelname = name;
+            sendEmail(userinfo.display_name, userinfo.real_name, userinfo.email, channelname, message.text);
+        })
 });
 
-web.users.list()
-    .then((res) => {
-        console.log(res);
-        res.members.forEach(m => user[m.id] = m);
-    })
-    .catch(console.error);
+function getUserInfo(id) {
+    return new Promise(function(resolve, reject) {
+        let info = user[id]
+        if (info) {
+            resolve(info);
+        } else {
+            web.users.profile.get({ user: id }).then((res) => {
+                // update the users roster
+                let profile = user[id] = res.profile;
+                resolve(profile);
+            }).catch(reject);
+        }
+    });
+}
 
+function getChanelname(id) {
+    return new Promise(function(resolve, reject) {
+        let info = channel[id]
+        if (info) {
+            resolve(info.name);
+        } else {
+            web.channels.info({ channel: id }).then((res) => {
+                // update the users roster
+                channel[id] = res.channel;
+                resolve(res.channel.name);
+            }).catch(reject);
+        }
+    });
+}
 
-
-function sendEmail(username, text) {
+function sendEmail(username, realname, email, channelname, text) {
     let mailOptions = {
-        from: '"SupportBot" <support@fxpal.com>', // sender address
-        to: 'support@fxpal.com', // list of receivers
-        subject: 'From Slack: #support', // Subject line
-        text: `${username} says: ${text}`, // plain text bod
+        from: mailFrom, // sender address
+        replyTo: `"${realname || mailTo}" <${email || mailTo}>`,
+        to: mailTo, // list of receivers
+        subject: 'From Slack: #' + channelname, // Subject line
+        text: `${username || 'Slack'} says: ${text}`, // plain text body
     };
-
-
+    console.log(mailOptions);
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
             return console.log(error);
